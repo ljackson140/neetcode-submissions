d960ade27907101e696b74e285dc3326fe08e28f
Laku Jackson <62328065+ljackson140@users.noreply.github.com>
2026-05-07 15:16:06 +1000

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) {
+
+        var freqMap = new Dictionary<int, int>();
+        var res = new List<int>();
+    
+        foreach (int num in nums) {
+            if(freqMap.ContainsKey(num)){
+                freqMap[num] = freqMap.GetValueOrDefault(num, 0) + 1;
+            }else{
+                freqMap[num] = 1;
+            }            
+        }
+
+        var sortedFreq = freqMap.OrderByDescending(x => x.Value).Take(k);
+        foreach(var count in sortedFreq){
+            res.Add(count.Key);
+        }
+
+        return res.ToArray();
+    
+   
+    }
+}
