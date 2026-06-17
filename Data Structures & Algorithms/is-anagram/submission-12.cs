fe8bdbac405587ee87765a460d9a0ba2c192b3c0
Laku Jackson <62328065+ljackson140@users.noreply.github.com>
2026-06-17 12:12:09 +1000

    Add: is-anagram - submission-12

---
+++ Data Structures & Algorithms/is-anagram/submission-12.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+
+        if(s.Length != t.Length) return false;
+
+        var frq = new int[26];
+
+        for(int i = 0; i < s.Length; i++){
+            frq[s[i] - 'a']++;
+            frq[t[i] - 'a']--;
+        }
+
+        foreach(var c in frq){
+           if(c != 0){
+            return false;
+           }
+        }
+
+        return true;
+
+    }
+}
